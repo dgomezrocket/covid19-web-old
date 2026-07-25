f25085d047191577c45382400d0fddf74692e49a
dgomezrocket <derlisrgomez@pol.una.py>
2026-07-25 12:15:02 -0300

    actualizar la URL del backend.

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,3 +1,6 @@
 /** @format */
 
-export const API_URL = "https://apisaludtotal.pol.una.py";
+/**export const API_URL = "https://apisaludtotal.pol.una.py"; */
+/**export const API_URL = "https://backend-core-covid19-production.up.railway.app"; */
+
+export const API_URL = process.env.REACT_APP_API_URL || "https://backend-core-covid19-production.up.railway.app";
